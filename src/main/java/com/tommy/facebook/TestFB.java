e582620e282eeff2b88deb864ac867e1023f2a8c
Tommy770221 <tommy770221@gmail.com>
2017-08-06 00:34:08 +0800

    1. FB相關爬取功能(新增大規模參數條件)

--- src/main/java/com/tommy/facebook/TestFB.java
+++ src/main/java/com/tommy/facebook/TestFB.java
@@ -8,7 +8,10 @@ import com.restfb.types.Page;
 import com.restfb.types.Post;
 
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
+
+import static java.lang.System.currentTimeMillis;
 
 /**
  * Created by Tommy on 2017/7/8.
@@ -36,9 +39,30 @@ public class TestFB {
                 System.out.println("------------------------------------------------------------------");
             }
         }
-
-        Connection<Message> messageConnection = facebookClient.fetchConnection("1582257328764467/feed", Message.class);
+        Date oneWeekAgo = new Date(currentTimeMillis() - 1000L * 60L * 60L * 24L * 7L);
+        Connection<Message> messageConnection = facebookClient.fetchConnection("1582257328764467/feed", Message.class,
+                Parameter.with("limit", 500), Parameter.with("until", "yesterday"),
+                Parameter.with("since", oneWeekAgo));
         List<Message> messageList=messageConnection.getData();
+        String nextURL=messageConnection.getNextPageUrl();
+        System.out.println("getNextPageUrl : "+messageConnection.getNextPageUrl());
+        System.out.println("getPreviousPageUrl : "+messageConnection.getPreviousPageUrl());
+        System.out.println("getAfterCursor : "+messageConnection.getAfterCursor());
+        System.out.println("getBeforeCursor : "+messageConnection.getBeforeCursor());
+
+      /*  Connection<Message> messageConnectionNext = facebookClient.fetchConnection(nextURL, Message.class);
+        for(Message message:messageConnectionNext.getData()){
+            System.out.println(message.getId());
+            Connection<JsonObject> eventComments = facebookClient.fetchConnection(message.getId()+"/comments", JsonObject.class);
+            for(JsonObject firstGroupPost:eventComments.getData()){
+                System.out.println("comments : "+firstGroupPost.get("message").asString());
+            }
+
+            Connection<JsonObject> eventLikes = facebookClient.fetchConnection(message.getId()+"/likes", JsonObject.class);
+            for(JsonObject firstGroupPost:eventLikes.getData()){
+                System.out.println("likes : "+firstGroupPost);
+            }
+        }*/
         for(Message message:messageList){
             System.out.println(message.getId());
             Connection<JsonObject> eventComments = facebookClient.fetchConnection(message.getId()+"/comments", JsonObject.class);
